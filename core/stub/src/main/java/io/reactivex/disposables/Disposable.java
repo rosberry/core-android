dabd70a0e166225e219297c2b70157ee84f81c40
AlexeyKorshun <lehakorshun@gmail.com>
2018-10-24 15:50:22 +0600

    AND-100: add paginator and stubs (#2)

---
+++ core/stub/src/main/java/io/reactivex/disposables/Disposable.java
@@ -0,0 +1,11 @@
+package io.reactivex.disposables;
+
+/**
+ * @author Alexei Korshun on 12/10/2018.
+ */
+public class Disposable {
+
+    public void dispose() {
+        throw new RuntimeException("You can't invoke this method, because this class is stub.");
+    }
+}
